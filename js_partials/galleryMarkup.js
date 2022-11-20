66f4efa38312c5486e2415e1bcf1cc10a710b85a
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2022-11-20 23:02:27 +0200

    feat: add css for gallery

--- js_partials/galleryMarkup.js
+++ js_partials/galleryMarkup.js
@@ -1,12 +1,13 @@
 import { refs, galleryContainer, galleryList } from "./refs.js";
 
-refs.main.classList.add("gallery");
+refs.main.classList.add("main");
+galleryList.classList.add("gallery");
 
 const galleryMarkup = (data) => {
   const itemMarkup = data
     .map((book) => {
       return `            <li class="gallery__item">
-                <img src="${book.imageLink}" alt="${book.title}" class="gallery_img">
+                <img src="${book.imageLink}" alt="${book.title}" class="gallery__img" draggable='true'>
                 <h2 class="gallery__title">${book.title}</h2>
                 <h3 class="gallery__author">by ${book.author}</h3>
                 <p class="gallery__price">Price: ${book.price}</p>
